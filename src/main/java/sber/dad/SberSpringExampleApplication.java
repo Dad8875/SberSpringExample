8a40b01d44b8439df60be51f1c2de366baf66ea4
Dad8875 <Serstep1956@yahoo.com>
2025-06-04 15:37:52 +0300

    initial commit

---
+++ src/main/java/sber/dad/SberSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package sber.dad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberSpringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SberSpringExampleApplication.class, args);
+    }
+
+}
